417f17d64e594236a3c87638b6e28790b7c447a7
Alp <67732998+alpblkba@users.noreply.github.com>
2026-06-16 01:39:51 +0200

    add lab2 task3 motion wakeup

---
+++ lab2/task3/Src/main.c
@@ -0,0 +1,352 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    GPIO/GPIO_IOToggle/Src/main.c
+  * @author  MCD Application Team
+  * @brief   Configure and toggle an on-board LED through the STM32U5xx HAL API.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include "b_u585i_iot02a_motion_sensors.h"
+#include "ism330dhcx.h"
+#include <string.h>
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN PV */
+UART_HandleTypeDef huart1;
+
+
+static volatile uint8_t motion_event = 0;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void SystemPower_Config(void);
+static void MX_ICACHE_Init(void);
+/* USER CODE BEGIN PFP */
+static void MX_USART1_UART_Init(void);
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+static void uart_print(const char *s) {
+  HAL_UART_Transmit(&huart1, (uint8_t *)s, strlen(s), HAL_MAX_DELAY);
+}
+
+static void motion_error(const char *msg) {
+  uart_print(msg);
+  uart_print("\\r\\n");
+
+  while (1) {
+    uart_print("motion sensor init failed\\r\\n");
+    HAL_Delay(1000);
+  }
+}
+
+
+static void MX_IMU_INT1_GPIO_Init(void) {
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  __HAL_RCC_GPIOE_CLK_ENABLE();
+
+  GPIO_InitStruct.Pin = GPIO_PIN_11;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+
+  HAL_NVIC_SetPriority(EXTI11_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI11_IRQn);
+}
+
+static void configure_motion_interrupt(void) {
+  ISM330DHCX_Object_t *obj =
+      (ISM330DHCX_Object_t *)Motion_Sensor_CompObj[0];
+
+  ism330dhcx_pin_int1_route_t int1_route = {0};
+
+  ism330dhcx_act_pin_notification_set(&obj->Ctx,
+                                      ISM330DHCX_DRIVE_SLEEP_CHG_EVENT);
+
+  ism330dhcx_act_mode_set(&obj->Ctx,
+                          ISM330DHCX_XL_AND_GY_NOT_AFFECTED);
+
+  ism330dhcx_wkup_threshold_set(&obj->Ctx, 2);
+
+  int1_route.md1_cfg.int1_sleep_change = 1;
+  ism330dhcx_pin_int1_route_set(&obj->Ctx, &int1_route);
+}
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void) {
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* Configure the system clock. */
+  SystemClock_Config();
+
+  /* Configure the system power. */
+  SystemPower_Config();
+
+  /* Initialize all configured peripherals. */
+  MX_ICACHE_Init();
+
+  /* USER CODE BEGIN 2 */
+  MX_USART1_UART_Init();
+  MX_IMU_INT1_GPIO_Init();
+
+  uart_print("\r\nLab2 Task3: wake on motion using IMU interrupt\r\n");
+
+  if (BSP_MOTION_SENSOR_Init(0, MOTION_ACCELERO) != BSP_ERROR_NONE) {
+    motion_error("BSP_MOTION_SENSOR_Init failed");
+  }
+
+  if (BSP_MOTION_SENSOR_Enable(0, MOTION_ACCELERO) != BSP_ERROR_NONE) {
+    motion_error("BSP_MOTION_SENSOR_Enable failed");
+  }
+
+  BSP_MOTION_SENSOR_SetOutputDataRate(0, MOTION_ACCELERO, 26.0f);
+  BSP_MOTION_SENSOR_SetFullScale(0, MOTION_ACCELERO, 2);
+
+  configure_motion_interrupt();
+
+  uart_print("move the board to trigger motion interrupt\r\n");
+/* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1) {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+    if (!motion_event) {
+      HAL_SuspendTick();
+      HAL_PWR_EnterSLEEPMode(PWR_MAINREGULATOR_ON, PWR_SLEEPENTRY_WFI);
+      HAL_ResumeTick();
+    }
+
+    if (motion_event) {
+      BSP_MOTION_SENSOR_Axes_t acc;
+      char msg[128];
+
+      motion_event = 0;
+
+      if (BSP_MOTION_SENSOR_GetAxes(0, MOTION_ACCELERO, &acc) == BSP_ERROR_NONE) {
+        snprintf(msg, sizeof(msg),
+                 "motion detected: x=%ld mg, y=%ld mg, z=%ld mg\r\n",
+                 acc.xval, acc.yval, acc.zval);
+        uart_print(msg);
+      } else {
+        uart_print("BSP_MOTION_SENSOR_GetAxes failed\r\n");
+      }
+
+      HAL_Delay(200);
+    }
+    /* USER CODE END 3 */
+  }
+  /* USER CODE END WHILE */
+}
+
+
+static void MX_USART1_UART_Init(void) {
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 115200;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart1.Init.ClockPrescaler = UART_PRESCALER_DIV1;
+  huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+
+  if (HAL_UART_Init(&huart1) != HAL_OK) {
+    Error_Handler();
+  }
+
+  if (HAL_UARTEx_SetTxFifoThreshold(&huart1, UART_TXFIFO_THRESHOLD_1_8) != HAL_OK) {
+    Error_Handler();
+  }
+
+  if (HAL_UARTEx_SetRxFifoThreshold(&huart1, UART_RXFIFO_THRESHOLD_1_8) != HAL_OK) {
+    Error_Handler();
+  }
+
+  if (HAL_UARTEx_DisableFifoMode(&huart1) != HAL_OK) {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+void EXTI11_IRQHandler(void) {
+  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_11);
+}
+
+void HAL_GPIO_EXTI_Rising_Callback(uint16_t GPIO_Pin) {
+  if (GPIO_Pin == GPIO_PIN_11) {
+    motion_event = 1;
+  }
+}
+/* USER CODE END 4 */
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void) {
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage. */
+  if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK) {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks. */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_MSI;
+  RCC_OscInitStruct.MSIState = RCC_MSI_ON;
+  RCC_OscInitStruct.MSICalibrationValue = RCC_MSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_4;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_MSI;
+  RCC_OscInitStruct.PLL.PLLMBOOST = RCC_PLLMBOOST_DIV1;
+  RCC_OscInitStruct.PLL.PLLM = 1;
+  RCC_OscInitStruct.PLL.PLLN = 80;
+  RCC_OscInitStruct.PLL.PLLP = 2;
+  RCC_OscInitStruct.PLL.PLLQ = 2;
+  RCC_OscInitStruct.PLL.PLLR = 2;
+  RCC_OscInitStruct.PLL.PLLRGE = RCC_PLLVCIRANGE_0;
+  RCC_OscInitStruct.PLL.PLLFRACN = 0;
+
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks. */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+                              | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2
+                              | RCC_CLOCKTYPE_PCLK3;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB3CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK) {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief Power Configuration
+  * @retval None
+  */
+static void SystemPower_Config(void) {
+  /*
+   * Disable the internal pull-up in dead battery pins of UCPD peripheral.
+   */
+  HAL_PWREx_DisableUCPDDeadBattery();
+
+  /*
+   * Switch to SMPS regulator instead of LDO.
+   */
+  if (HAL_PWREx_ConfigSupply(PWR_SMPS_SUPPLY) != HAL_OK) {
+    Error_Handler();
+  }
+
+  /* USER CODE BEGIN PWR */
+  /* USER CODE END PWR */
+}
+
+/**
+  * @brief ICACHE Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_ICACHE_Init(void) {
+  /* USER CODE BEGIN ICACHE_Init 0 */
+  /* USER CODE END ICACHE_Init 0 */
+
+  /* USER CODE BEGIN ICACHE_Init 1 */
+  /* USER CODE END ICACHE_Init 1 */
+
+  /** Enable instruction cache in 1-way direct mapped cache mode. */
+  if (HAL_ICACHE_ConfigAssociativityMode(ICACHE_1WAY) != HAL_OK) {
+    Error_Handler();
+  }
+
+  if (HAL_ICACHE_Enable() != HAL_OK) {
+    Error_Handler();
+  }
+
+  /* USER CODE BEGIN ICACHE_Init 2 */
+  /* USER CODE END ICACHE_Init 2 */
+}
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void) {
+  /* USER CODE BEGIN Error_Handler_Debug */
+  while (1) {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line) {
+  /* USER CODE BEGIN 6 */
+  while (1) {
+  }
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
